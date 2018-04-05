9962ed5a68c94158ffdd0c4bbb4b605a879b67f7
Mirza Cengic <mirzaceng@gmail.com>
2018-04-05 15:55:40 +0200

    Added function documentation

--- R/GDAL_crop.R
+++ R/GDAL_crop.R
@@ -1,22 +1,48 @@
 # GDAL_crop function draft #####################
 
-# infile - input raster
-# outfile - output raster path
-# shp - shapfile path
-# return raster - set TRUE to load output
-GDAL_crop <- function(infile, outfile, shp, return_raster = FALSE)
+#' Crop raster
+#'
+#' The function crops rasters (to the shapefile boundary) using GDAL.
+#'
+#' @param input_raster Raster to be cropped (file path or Raster object). Input raster should be stored on disk for GDAL access.
+#' @param filename Output raster path.
+#' @param shapefile_path Shapefile that is used to crop the raster. Disk path, \strong{not sp or sf object}.
+#' @param return_raster Logical. The function stores the raster in the \code{filename} argument path as a side-effect.
+#'
+#' @return
+#' @export
+#'
+#' @importFrom pkgmaker file_extension
+#' @importFrom raster raster
+#' @examples
+#' library(gdalR)
+#' library(Rahat)
+#'
+#' we_eu <- milkunize("Projects/Land_use/Data/Shapefile/IMAGE_regions/Individual_regions/Western_Europe_IMAGE.shp")
+#' dem <- milkunize("Merit_DEM_10s.tif", "m5")
+#'
+#' # Crop dem
+#' dem_cropped <- GDAL_crop(dem, filename = "/vol/milkun1/Mirza_Cengic/Temp/dem_we.tif", shapefile_path = we_eu)
+
+GDAL_crop <- function(input_raster, filename, shapefile_path, return_raster = TRUE)
 {
+
+  if (missing(filename))
+  {
+    filename <- tempfile(fileext = ".tif")
+    warning("Argument filename is missing. Temporary raster file will be created.")
+  }
   # Set vrt filename
-  outfile_vrt <- gsub(pkgmaker::file_extension(outfile), "vrt", outfile)
-  
-  	if (inherits(infile, "Raster"))
+  outfile_vrt <- gsub(pkgmaker::file_extension(filename), "vrt", filename)
+
+  	if (inherits(input_raster, "Raster"))
 {
-  infile <- infile@file@name
- } 
-  
+  input_raster <- input_raster@file@name
+ }
+
   # Make system calls
-  cut_to_VRT <- paste0("gdalwarp -multi -cutline", " ", shp, " ", "-crop_to_cutline -of vrt", " ", infile, " ", outfile_vrt)
-  VRT2TIF <- paste0("gdal_translate -co compress=LZW", " ", outfile_vrt, " ", outfile)
+  cut_to_VRT <- paste0("gdalwarp -multi -cutline", " ", shapefile_path, " ", "-crop_to_cutline -of vrt", " ", input_raster, " ", outfile_vrt)
+  VRT2TIF <- paste0("gdal_translate -co compress=LZW", " ", outfile_vrt, " ", filename)
 
   # Run sytem calls
   system(cut_to_VRT)
@@ -24,9 +50,10 @@ GDAL_crop <- function(infile, outfile, shp, return_raster = FALSE)
   # Remove vrt file
   unlink(outfile_vrt)
 
-  if (isTRUE(return_raster)) {
+  if (return_raster)
+  {
     library(raster)
-    out <- raster::raster(outfile)
+    out <- raster::raster(filename)
     return(out)
   }
 }
